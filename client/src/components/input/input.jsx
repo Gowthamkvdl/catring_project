7416e7df18dd4b35010fc19fc93f7f8976225bcc
gowthamkvdl <gowthamkvdl@gmail.com>
2024-07-26 16:11:56 +0530

    gtrf

--- client/src/components/input/input.jsx
+++ client/src/components/input/input.jsx
@@ -113,7 +113,7 @@ const input = () => {
                 xmlns="http://www.w3.org/2000/svg"
                 width="25"
                 height="25"
-                fill="currentColor"
+                fill="yellow"
                 class="bi bi-search"
                 viewBox="0 0 16 16"
               >
