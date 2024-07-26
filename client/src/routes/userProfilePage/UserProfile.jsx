dd6431615acd771086ed349f7f52092a82d0e2c3
gowthamkvdl <gowthamkvdl@gmail.com>
2024-07-26 18:25:34 +0530

    bvnm

--- client/src/routes/userProfilePage/UserProfile.jsx
+++ client/src/routes/userProfilePage/UserProfile.jsx
@@ -78,22 +78,7 @@ const profilePage = () => {
       <div className="row ">
         <div className="col-12 col-md-6">
           <BackBtn color="white" />
-          <h2 className="title text-uppercase">
-            {user.username}'S PROFILE
-            <button className="btn btn-warning float-end">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                fill="currentColor"
-                class="bi bi-star-half mb-1 me-1"
-                viewBox="0 0 16 16"
-              >
-                <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z" />
-              </svg>
-              Provide feedback
-            </button>
-          </h2>
+          <h2 className="title text-uppercase">{user.username}'S PROFILE</h2>
           <div className="profile bg-light text-dark mx-2 mx-md-0 py-4 rounded-4 box-shadow mt-4 row">
             <div className="profilePic  d-flex p-5 pb-3 pt-2 p-md-0 pb-md-0  flex-column col-12 col-md-5">
               <img
@@ -145,21 +130,37 @@ const profilePage = () => {
               </div>
             </div>
           </div>
-            <div className="">
-              <button onClick={handleAddChat} className="btn mt-3 w-100 btn-warning">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="bi bi-send me-2"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
-                </svg>
-                Send Message
-              </button>
-            </div>
+          <div className="">
+            <button
+              onClick={handleAddChat}
+              className="btn mt-3 w-100 btn-warning"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-send me-2"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+              </svg>
+              Send Message
+            </button>
+            <button className="btn btn-warning w-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                class="bi bi-star-half mb-1 me-1"
+                viewBox="0 0 16 16"
+              >
+                <path d="M5.354 5.119 7.538.792A.52.52 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.54.54 0 0 1 16 6.32a.55.55 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.5.5 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.6.6 0 0 1 .085-.302.51.51 0 0 1 .37-.245zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.56.56 0 0 1 .162-.505l2.907-2.77-4.052-.576a.53.53 0 0 1-.393-.288L8.001 2.223 8 2.226z" />
+              </svg>
+              Provide feedback
+            </button>
+          </div>
         </div>
         <div className="col-12 col-md-6 mb-4 mx-0 mt-5 mt-md-0">
           <h4 className="text-uppercase">{user.username}'s Events</h4>
