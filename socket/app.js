d5db815f7cce010d20848fec9c3cc399dc7db201
gowthamkvdl <gowthamkvdl@gmail.com>
2024-07-26 22:44:40 +0530

    dgdrhy

--- socket/app.js
+++ socket/app.js
@@ -2,7 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "https://cartringboys.netlify.app",
+    origin: "https://catringboys.netlify.app",
   },
 });
 
