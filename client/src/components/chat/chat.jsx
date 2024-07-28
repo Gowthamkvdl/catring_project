4cef9aebefa442b6ef3d79784f29172d10463cf9
gowthamkvdl <gowthamkvdl@gmail.com>
2024-07-28 12:17:43 +0530

    fghg

--- client/src/components/chat/chat.jsx
+++ client/src/components/chat/chat.jsx
@@ -8,6 +8,7 @@ import { SocketContext } from "../../context/SocketContext";
 import apiRequest from "../../lib/apiRequest";
 import { toast } from "react-hot-toast";
 import Loader from "../../components/loader/Loader";
+import rollingLoading from "../../assets/rollingLoading.svg";
 
 const Chat = ({ items }) => {
   const [itemsArray, setItemsArray] = useState(items);
@@ -383,14 +384,15 @@ const Chat = ({ items }) => {
                   title="Send"
                   className="float-end btn btn-warning"
                 >
-                  <svg
+                  {sending && rollingLoading}
+                  {!sending && <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="16"
                     height="16"
                     fill="currentColor"
                     className="bi bi-send-fill"
                     viewBox="0 0 16 16"
-                  >
+                  >}
                     <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
                   </svg>
                 </button>
