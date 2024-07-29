65ccbecb88ede669a7f7067e6a57836e17af1f66
gowthamkvdl <gowthamkvdl@gmail.com>
2024-07-29 16:36:27 +0530

    ghmkhk

--- client/src/components/draggableMarker/DraggableMarker.jsx
+++ client/src/components/draggableMarker/DraggableMarker.jsx
@@ -1,6 +1,18 @@
 // draggableMarker/DraggableMarker.jsx
 import React, { useState, useRef, useMemo, useCallback } from "react";
 import { Marker, Popup } from "react-leaflet";
+import pin from "../../assets/pin.png";
+
+// Define a custom icon
+const customIcon = L.icon({
+  iconUrl: pin, // Path to your marker icon
+  iconSize: [40, 40], // size of the icon
+  // shadowUrl: require("./path/to/your/marker-shadow.png"), // Path to your marker shadow
+  // shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+});
+
 
 const DraggableMarker = ({ center, onDragEnd }) => {
   const [position, setPosition] = useState(center);
@@ -30,6 +42,7 @@ const DraggableMarker = ({ center, onDragEnd }) => {
       eventHandlers={eventHandlers}
       position={position}
       ref={markerRef}
+      icon={customIcon}
     >
       <Popup minWidth={90}>
         <span onClick={toggleDraggable}>Drag me to select a location</span>
--- client/src/components/singlePointerMap/SinglePointerMap.jsx
+++ client/src/components/singlePointerMap/SinglePointerMap.jsx
@@ -3,7 +3,17 @@ import "./singlePointerMap.css";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import pin from "../../assets/pin.png";
 
+// Define a custom icon
+const customIcon = L.icon({
+  iconUrl: pin, // Path to your marker icon
+  iconSize: [40, 40], // size of the icon
+  // shadowUrl: require("./path/to/your/marker-shadow.png"), // Path to your marker shadow
+  // shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  popupAnchor: [1, -34], // point from which the popup should open relative to the iconAnchor
+});
 
 const Map = ({ latitude, longitude }) => {
   return (
@@ -17,7 +27,7 @@ const Map = ({ latitude, longitude }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]}></Marker>
+      <Marker position={[latitude, longitude]} icon={customIcon}></Marker>
     </MapContainer>
   );
 };
