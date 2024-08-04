0734132983ae4416ec554e7fe194f76ab277bc11
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-04 17:40:09 +0530

    erger

--- api/routes/message.route.js
+++ api/routes/message.route.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
 import {
-  addMessage
+  addMessage,
+  deleteMessages,
 } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.post("/:id",verifyToken, addMessage);
+router.delete("/",verifyToken, deleteMessages);
 
 
 export default router    
