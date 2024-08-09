295da25ea00a4ec96545b97809cc470da96cb848
gowthamkvdl <gowthamkvdl@gmail.com>
2024-08-09 13:28:39 +0530

    tjrty

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://cateringboys.onrender.com/api",
+  baseURL: "https://catringboys.onrender.com/api",
   withCredentials: true,
 });
 
